525131a5c9272b1927a6d428c1b517fe827d7f11
Omer Ahmed <om3r.ahmed@yandex.com>
2022-10-13 13:45:05 -0700

    initial

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,5 @@
-uth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
 
- /**
-  *  * main - Prints "Programming is like building a multilingual
-  *   *                puzzle, followed by a new line.
-  *    *
-  *     * Return: Always 0.
-  *      */
      int main (void)
 {
 		printf("Programming is like building a multilingual puzzle \n");
