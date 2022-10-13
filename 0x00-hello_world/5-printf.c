13d586e8aa4fef0e995fa02c5680b0e9427efdf6
Omer Ahmed <om3r.ahmed@yandex.com>
2022-10-13 15:56:26 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 /*
- *  * File: 5-printf.c
- *   * Auth: Brennan D Baraban
- *    */
+ *   File: 5-printf.c
+ *   Auth: Brennan D Baraban
+*/
 #include <stdio.h>
-     int main(void)
+int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
 	
