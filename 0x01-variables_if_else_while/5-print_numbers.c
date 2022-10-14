ce70f7b8f06af68e5a4dca8a5f293b0c18ff997a
Omer Ahmed <om3r.ahmed@yandex.com>
2022-10-14 16:15:43 -0700

    inital

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The main thing of this code
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
