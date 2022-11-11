587434f232787f358707dc9a6af0a1852cb50e3e
Omer Ahmed <om3r.ahmed@yandex.com>
2022-11-11 10:53:25 -0800

    init

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _strspn - gets the length of a prefix substring
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -15,6 +24,7 @@ int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
